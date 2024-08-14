ec011d7bf0c76331fb34bb97f76cd5074d307b7a
shftee <shifath.hossain2@gmail.com>
2024-08-14 16:07:57 -0400

    loaded modules

---
+++ app/api/checkout_sessions.js
@@ -0,0 +1,37 @@
+import { NextResponse } from 'next/server'
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2022-11-15',
+})
+
+export async function POST(req) {
+  try {
+    const params = {
+      mode: 'subscription',
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'usd',
+            product_data: {
+              name: 'Pro subscription',
+            },
+            unit_amount: 1000, // $10 in cents
+            recurring: {
+              interval: 'month',
+              interval_count: 1,
+            },
+          },
+          quantity: 1,
+        }
+      ]
+
+    }
+  } catch (error) {
+    console.error('Error creating checkout session:', error)
+    return new NextResponse(JSON.stringify({ error: { message: error.message } }), {
+      status: 500,
+    })
+  }
+}
