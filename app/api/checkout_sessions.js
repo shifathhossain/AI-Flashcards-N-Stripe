8f58b0e7f46b7b7b49082aac0bff2f5f8cf04ff7
shftee <shifath.hossain2@gmail.com>
2024-08-14 20:07:28 -0400

    page update

--- app/api/checkout_sessions.js
+++ app/api/checkout_sessions.js
@@ -1,3 +1,7 @@
+const formatAmountForStripe = (amount, currency) => {
+  return Math.round(amount * 100)
+ }
+
 import { NextResponse } from 'next/server'
 import Stripe from 'stripe'
 
@@ -17,21 +21,50 @@ export async function POST(req) {
             product_data: {
               name: 'Pro subscription',
             },
-            unit_amount: 1000, // $10 in cents
+            unit_amount: formatAmountForStripe(10, 'usd'), // $10.00
             recurring: {
               interval: 'month',
               interval_count: 1,
             },
           },
           quantity: 1,
-        }
-      ]
-
+        },
+      ],
+      success_url: `${req.headers.get(
+        'Referer',
+      )}result?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${req.headers.get(
+        'Referer',
+      )}result?session_id={CHECKOUT_SESSION_ID}`,
     }
+    
+    const checkoutSession = await stripe.checkout.sessions.create(params)
+    
+    return NextResponse.json(checkoutSession, {
+      status: 200,
+    })
   } catch (error) {
     console.error('Error creating checkout session:', error)
     return new NextResponse(JSON.stringify({ error: { message: error.message } }), {
       status: 500,
     })
+  }
+}
+
+export async function GET(req) {
+  const searchParams = req.nextUrl.searchParams
+  const session_id = searchParams.get('session_id')
+
+  try {
+    if (!session_id) {
+      throw new Error('Session ID is required.')
+    }
+    const checkoutSession = await stripe.checkout.sessions.retrieve(session_id)
+
+    return NextResponse.json(checkoutSession)
+
+  } catch (error) {
+    consol.error('Error retrieving checkout session: ', error)
+    return NextResponse.json({error: {message: error.message}}, {status:500})
   }
 }
